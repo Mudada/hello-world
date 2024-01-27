b4fe18b8b018904d1858f00367a26ce833124c79
Mudada <mael.nicolas77@gmail.com>
2024-01-27 17:44:33 +0100

    Seems like its working hehe

--- flake.nix
+++ flake.nix
@@ -1,11 +1,22 @@
 {
-  description = "A very basic flake";
+  description = "Zig Hello World";
 
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable" ;
   };
+
+  outputs = { self, nixpkgs }:
+
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in 
+    {
+      devShells.x86_64-linux.default = 
+	pkgs.mkShell {
+	  packages = with pkgs; [
+	    zig
+	  ];
+	};
+    };
 }
